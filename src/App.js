519c4b9e1adac8761289826606b6f94c9fa29ffa
Anthony Climer <anthonyclimer0925@gmail.com>
2023-12-01 18:11:30 -0500

    Added the second to last bit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ function App() {
             <span aria-hidden='true'>00</span>Home
           </a>
         </li>
+        <li>
+          <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
+            <span aria-hidden='true'>01</span>Destination
+          </a>
+        </li>
+        <li>
+          <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
+            <span aria-hidden='true'>02</span>Crew
+          </a>
+        </li>
       </ul>
     </nav>
   );
