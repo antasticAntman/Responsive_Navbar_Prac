a77ce01947b8503bc55edeeb9ccb051f76776d5e
Anthony Climer <anthonyclimer0925@gmail.com>
2023-12-01 18:48:40 -0500

    Added the logo

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import './App.css';
 
 function App() {
   return (
-    <nav>
-      <ul id='primary-navigation' class='primary-navigation flex'>
-        <li class='active'>
-          <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
-            <span aria-hidden='true'>00</span>Home
-          </a>
-        </li>
-        <li>
-          <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
-            <span aria-hidden='true'>01</span>Destination
-          </a>
-        </li>
-        <li>
-          <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
-            <span aria-hidden='true'>02</span>Crew
-          </a>
-        </li>
-        <li>
-          <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
-            <span aria-hidden='true'>03</span>Technology
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <header class='primary-header'> 
+      <div >
+        <img className='Cat-logo' src='https://toppng.com/uploads/preview/free-download-cat-png-vector-icon-cat-transparent-background-cat-icon-transparent-background-11563035549ypnlilorb5.png' alt='logo'/>
+      </div>
+
+      <nav>
+        <ul id='primary-navigation' class='primary-navigation flex'>
+          <li class='active'>
+            <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
+              <span aria-hidden='true'>00</span>Home
+            </a>
+          </li>
+          <li>
+            <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
+              <span aria-hidden='true'>01</span>Destination
+            </a>
+          </li>
+          <li>
+            <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
+              <span aria-hidden='true'>02</span>Crew
+            </a>
+          </li>
+          <li>
+            <a class='ff-sans-cond uppercase text-white letter-spacing-2' href='#'>
+              <span aria-hidden='true'>03</span>Technology
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
   );
 }
 
